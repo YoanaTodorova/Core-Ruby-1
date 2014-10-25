d38517fbe6c991566c46698e9af5f2c7b738122c
Yoana Todorova <yoana.todorova@conit-bg.com>
2014-10-25 20:07:51 +0300

    solutions for fmi-tribute problems

--- week1/1-fmi-tribute/solution.rb
+++ week1/1-fmi-tribute/solution.rb
@@ -1,3 +1,17 @@
 class Array
-  # Your code goes here.
+  def to_hash
+    Hash[*(map { |e| e.empty? ? [[]] : e }.flatten(1))]
+  end
+
+  def index_by(&block)
+    Hash[map(&block).zip(self)]
+  end
+
+  def occurences_count
+    Hash.new(0).tap { |hsh| each { |el| hsh[el] += 1 } }
+  end
+
+  def subarray_count(subarray)
+    each_cons(subarray.length).to_a.count(subarray)
+  end
 end
