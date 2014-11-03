c1f782b5f57b3e2ee351c324063742bbfd4e4bd7
Yoana Todorova <yoana.todorova@conit-bg.com>
2014-11-03 17:10:26 +0200

    solutions for problems with hashes

--- week1/2-hashes/solution.rb
+++ week1/2-hashes/solution.rb
@@ -1,3 +1,41 @@
 class Hash
-  # Your code goes here.
+  def pick(*keys)
+    select { |key,value| keys.include? key }
+  end
+
+  def pick!(*keys)
+    select! { |key,value| keys.include? key }
+  end
+
+  def except(*keys)
+    reject { |key,value| keys.include? key }
+  end
+
+  def except!(*keys)
+    reject! { |key,value| keys.include? key }
+  end
+
+  def values_map(&block)
+    Hash[keys.zip(values.map(&block))]
+  end
+
+  def values_map!(&block)
+    merge! Hash[keys.zip(values.map(&block))]
+  end
+
+  def compact_values
+    reject { |key,value| !!value == false }
+  end
+
+  def compact_values!
+    reject! { |key,value| !!value == false }
+  end
+
+  def defaults(hash)
+    merge(hash) { |key, oldval, newval| oldval }
+  end
+
+  def defaults!(hash)
+    merge!(hash) { |key, oldval, newval| oldval }
+  end
 end
