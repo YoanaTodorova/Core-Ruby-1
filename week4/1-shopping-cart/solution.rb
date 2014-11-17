c05de8703d4ab0dc6680fa2feee3f570b1831aec
Yoana Todorova <yoana.todorova@conit-bg.com>
2014-11-18 00:36:43 +0200

    invoice and cart

--- week4/1-shopping-cart/solution.rb
+++ week4/1-shopping-cart/solution.rb
@@ -1,12 +1,54 @@
+require 'bigdecimal'
+require 'bigdecimal/util'
+
 class Inventory
+  attr_accessor :products
+
   def initialize
     @products = {}
   end
  
   def register(product, price)
     raise 'Invalid parameters passed' if product.length > 40 ||
-                                         @products[product] ||
+                                         @products[product]  ||
                                          !(0.01..999.99).include?(price.to_f)
     @products[product] = price
+  end
+
+  def new_cart
+    Cart.new(self)
+  end
+end
+
+
+class Cart
+  def initialize(inventory)
+    @inventory = inventory
+    @products = Hash.new(0)
+  end
+
+  def add(product, quantity = 1)
+    raise 'Invalid product passed' unless @inventory.products[product]
+    raise 'Invalid quantity'           if quantity < 1
+
+    @products[product] += quantity
+  end
+
+  def total
+    @products.map { |product, quantity| @inventory.products[product].to_d * quantity }
+             .reduce(:+)
+  end
+
+  def invoice
+    invoice = ['+------------------------------------------------+----------+',
+               '| Name                                       qty |    price |',
+               '+------------------------------------------------+----------+']
+    @products.each do |product, quantity|
+      invoice += ["| #{product.ljust(40)} #{quantity.rjust(5)} |#{('%0.2f' % @inventory.products[product]).rjust(10)}|",
+                  '+------------------------------------------------+----------+']
+    end
+    invoice += ["| #{'TOTAL'.ljust(41)} |#{('%0.2f' % total).rjust(10)}|",
+                '+------------------------------------------------+----------+']
+    invoice.join "\n"
   end
 end
--- week4/1-shopping-cart/solution_test.rb
+++ week4/1-shopping-cart/solution_test.rb
@@ -1,4 +1,6 @@
 require 'minitest/autorun'
+require 'bigdecimal'
+require 'bigdecimal/util'
 
 require_relative 'solution'
 
@@ -11,4 +13,21 @@ class SolutionTest < Minitest::Test
     assert_raises(RuntimeError) { inventory.register 'Green Tea', '1.00' }
     assert_raises(RuntimeError) { inventory.register 'Green Teaa', '0.001' }
   end
+
+  def test_total
+  inventory = Inventory.new
+  inventory.register 'Green Tea', '1.99'
+  inventory.register 'Red Tea',   '2.49'
+  inventory.register 'Earl Grey', '1.49'
+
+  cart = inventory.new_cart
+
+  cart.add 'Green Tea'
+  cart.add 'Red Tea', 2
+  cart.add 'Green Tea', 2
+
+  cart.total # връща '10.95'.to_d 
+  assert_equal '10.95'.to_d, cart.total
+  end
 end
+
