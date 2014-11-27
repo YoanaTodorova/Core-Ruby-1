a5a7a077da6d90058913ad94b27929f44675ea02
Yoana Todorova <yoana.todorova@conit-bg.com>
2014-11-27 20:07:10 +0200

    String#to_proc

---
+++ week5/1-meta/solution.rb
@@ -0,0 +1,10 @@
+class String
+  def to_proc
+    methods = split('.')
+    proc do |argument|
+      methods.inject(argument) do |result, method|
+        result.public_send(method)
+      end
+    end
+  end
+end
--- week5/1-meta/solution_test.rb
+++ week5/1-meta/solution_test.rb
@@ -3,7 +3,7 @@ require 'minitest/autorun'
 require_relative 'solution'
 
 class SolutionTest < Minitest::Test
-  def test_the_truth
-    assert true
+  def test_to_proc
+    assert_equal [3,4], [1,2].map(&'succ.succ')
   end
 end
