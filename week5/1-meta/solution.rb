2ef91015d4e30efe11ada685abc42bf3cfd26ce8
Yoana Todorova <yoana.todorova@conit-bg.com>
2014-12-02 19:42:17 +0200

    proxy & delegate

--- week5/1-meta/solution.rb
+++ week5/1-meta/solution.rb
@@ -16,7 +16,29 @@ class NilClass
 end
 
 class Object
-  def define_singleton_method(name, &implementation)
-    singleton_class.send(:define_method, name, &implementation)
+  def singleton_class
+    self
+  end
+
+  # def define_singleton_method(name, &implementation)
+  #   singleton_class.send(:define_method, name, &implementation)
+  # end
+
+  def self.delegate(method, to: object)
+    self.class_eval do
+      define_singleton_method method do
+        self.instance_variable_get(object).public_send(method)
+      end
+    end
+  end
+end
+
+class Proxy < Object
+  def initialize(target)
+    @target = target
+  end
+
+  def method_missing(name, *args, &block)
+    @target.public_send(name, *args, &block)
   end
 end
--- week5/1-meta/solution_test.rb
+++ week5/1-meta/solution_test.rb
@@ -18,4 +18,34 @@ class SolutionTest < Minitest::Test
 
     assert_raises(NoMethodError) { klass.m }
   end
+
+  def test_singleton_class
+    klass = Class.new
+    a = klass.new
+    a.singleton_class
+  end
+
+  def test_proxy
+    proxy = Proxy.new [1,2,3]
+    assert_equal [2], proxy & [2]
+  end
+
+  def test_delegate
+    struct = Struct.new(:first_name, :last_name)
+
+    i = Class.new do
+      delegate :fist_name, to: '@user'
+      delegate :last_name, to: '@user'
+
+      def initialize(user)
+        @user = user
+      end
+    end
+
+    user = struct.new 'Genadi', 'Samokovarov'
+    invoice = i.new(user)
+
+    assert_equal 'Genadi', user.first_name
+    assert_equal 'Samokovarov', user.last_name
+  end
 end
